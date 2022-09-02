0697b2f25ba53dd7882eb45cdb0552074c3673ea
emmathomas36 <eet2341@rit.edu>
2022-09-02 14:03:35 -0400

    structure of page

---
+++ my-app/src/data.js
@@ -0,0 +1,62 @@
+import myLife from './images/mylife-cover.png'
+export const personalInfo = [
+    {
+        name: "Emma Thomas",
+        personalDescription: "I am a third year student athlete at Rochester Institute of Technology majoring in Human-Centered Computing with a minor in Web Development. " +
+                            "Within my major, I am concentrating on front-end development and UX/UI design."
+                                
+    }
+]
+export const techProjectData = [
+    {
+        projectName: "SpotifyMatched",
+        mainImage: "",
+        slideImages: [],
+        description: "",
+        course: "",
+        date: ""
+    },
+    {
+        projectName: "RIT myLife Redesign",
+        mainImage: myLife,
+        slideImages: [],
+        description: "",
+        course: "",
+        date: ""
+    },
+    {
+        projectName: "iSchool Web App",
+        mainImage: "",
+        slideImages: [],
+        description: "",
+        course: "",
+        date: ""
+    },
+    {
+        projectName: "Flutter Recipe App",
+        mainImage: "",
+        slideImages: [],
+        description: "",
+        course: "",
+        date: ""
+    },
+]
+
+export const designProjectData = [
+    {
+        projectName: "Graphic Translation",
+        mainImage: "",
+        slideImages: [],
+        description: "",
+        course: "",
+        date: ""
+    },
+    {
+        projectName: "Figma Playlist Covers",
+        mainImage: "",
+        slideImages: [],
+        description: "",
+        course: "",
+        date: ""
+    }
+]
--- my-app/src/home-grid.js
+++ my-app/src/home-grid.js
@@ -1,14 +1,30 @@
+import { personalInfo, techProjectData } from "./data";
 export const HomeGrid = () => {
     return (
         <div>
-        <div className = "headingp-8 m-10">
-            <h1 className = "text-5xl font-light">Emma Thomas Portfolio</h1>
+            <div className = "content-center bg-gray-200 p-4">
+                <h1 className = "w-1/2 text-5xl text-left font-light">{personalInfo[0].name}</h1>
+                <div className = "w-1/2 text-right float-right">
+                    <a className = "p-4" href = "">Resume</a>
+                    <a className = "p-4" href = "www.linkedin.com/in/emma-e-thomas">LinkedIn</a>
+                    </div>
+                <p className = "w-1/2 text-left">
+                    {personalInfo[0].personalDescription}
+                </p>
             </div>
-
-            <div class="grid grid-cols-2 gap-20 m-16">
-            <div className = "container bg-gray-200 rounded h-64 ">Project 1</div>
-            <div className = "container bg-gray-200 rounded h-64" >Project 2</div>
-        </div>
+            <h3 className = "text-3xl text-left font-bold mx-8 p-4">Technical Projects</h3>
+            <div class="grid grid-cols-2 gap-10 mx-16 my-8">
+                {techProjectData.map((data, key) => {
+                    return (
+                        <div key = {key} className = "container bg-gray-200 rounded h-80 shadow-lg">
+                            <h2 className = "text-2xl font-light p-4">{data.projectName}</h2>
+                            <div className = "flex content justify-center">
+                            <img src = { data.mainImage} className = "h-56 content-evenly"></img>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
